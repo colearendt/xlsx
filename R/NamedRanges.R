1110df5562dd935d80891142a597fbab4f0bd7e5
adrian.dragulescu <adrian.dragulescu@gmail.com>
2012-04-25 19:16:46 +0000

    incorporate Alexey's changes

--- R/NamedRanges.R
+++ R/NamedRanges.R
@@ -65,7 +65,7 @@ readRange <- function(range, sheet, colClasses = "character")
   if (length(colClasses) < noColumns)
     colClasses <- rep(colClasses, noColumns)
   
-  Rintf <- .jnew("dev/RInterface")  # create an interface object 
+  Rintf <- .jnew("org/cran/rexcel/RInterface")  # create an interface object 
   res <- vector("list", length=noColumns)
   for (i in seq_len(noColumns)){
     res[[i]] <- switch(colClasses[i],
@@ -115,86 +115,3 @@ createRange <- function(rangeName, firstCell, lastCell)
   ## nameFormula <- areaRef$formatAsString()
 
 
-## #############################################################################
-## # some old code not needed
-## #
-## writeRange <- function(wb, file, x, range, colNames = FALSE)
-## {
-##   if (!is.data.frame(x))
-##     stop("No data.frame", call. = FALSE)
-  
-##   rangeIdx <- .jcall(wb, "I", "getNameIndex", range)
-##   if (rangeIdx == -1)
-##     stop("Range not found", call. = FALSE)
-  
-##   aRange <- .jcall(wb, "Lorg/apache/poi/hssf/usermodel/HSSFName;",
-##     "getNameAt", rangeIdx)
-##   if (.jcall(aRange, "Z", "isDeleted"))
-##     stop(paste(range, "has no cell reference"), call. = FALSE)
-  
-##   ## getRefersToFormula throws an error if external reference is broken
-##   aRangeRef <- tryCatch(.jcall(aRange, "S", "getRefersToFormula"),
-##     error = function(e) stop("Invalid range address", call. = FALSE))
-  
-##   ## A broken reference starts with #REF!
-##   if (grepl("#REF!", aRangeRef, fixed=TRUE) > 0)
-##     stop(paste(range, aRangeRef, "is broken"), call. = FALSE)
-  
-##   ## A linked range looks like '[path\path\file.xls]Sheet'!A1:B1
-##   if (grepl("[", aRangeRef, fixed=TRUE) > 0)
-##     stop(paste(range, aRangeRef, "linked range can't be read'"), call. = FALSE)
-##   if (!.jcall("org/apache/poi/ss/util/AreaReference", "Z", "isContiguous", aRangeRef))
-##     stop(paste(range, "is not contiguous"), call. = FALSE)
-    
-##   iOffset <- as.integer(colNames)
-
-##   Rintf <- .jnew("dev/RInterface2")
-##   warnings <- Rintf$prepCELL_ARRAY(wb, range, as.integer(nrow(x) + iOffset), as.integer(ncol(x)))
-##   ## warnings[1] = noRowsDf - noRowsRg, warnings[2] = noColsDf - noColsRg
-##   if (warnings[1] != 0)
-##     print(paste("Different row lengths: Data.frame", nrow(x), "Range", nrow(x) - warnings[1]))
-##   if (warnings[2] != 0)
-##     print(paste("Different col lengths: Data.frame", ncol(x), "Range", ncol(x) - warnings[2]))
-
-##   if (colNames)
-##     .jcall(Rintf, "V", "writeRowStrings", 0L, 0L, .jarray(names(x)))
-
-
-##   ## write one column at a time
-##   ## if a cell has a formula, the formula will NOT be overwritten
-##   for (j in 1:ncol(x)) {
-##     if (class(x[,j]) == "integer") {
-##       aux <- .jarray(x[,j])
-##       .jcall(Rintf, "V", "writeColInts", iOffset, as.integer(j-1), aux)
-##     } else if (class(x[,j]) == "numeric") {
-##       aux <- .jarray(x[,j])
-##       .jcall(Rintf, "V", "writeColDoubles", iOffset, as.integer(j-1), aux)
-##     } else {
-##       aux <- .jarray(as.character(x[,j]))
-##       .jcall(Rintf, "V", "writeColStrings", iOffset, as.integer(j-1), aux)
-##     }
-##   }
-
-##   fh <- .jnew("java/io/FileOutputStream", file)  # open the file
-##   wb$write(fh)                                   # write the spreadsheet
-##   .jcall(fh, "V", "close")                       # close the file
-  
-##   invisible()
-## }
-
-
-
-
-  ##   res <- regexpr("\\[.*\\]", aRange$toString())   # why is this here?
-  ##   ranges[i] <- paste(substr(s, res+1, res + attr(res, "match.length") - 2), aRangeRef)
-  ##   ranges[i] <- aRangeRef
-  ## }
-  
-
-  ## rangeIdx <- .jcall(wb, "I", "getNameIndex", range)
-  ## if (rangeIdx == -1)
-  ##   stop("Range not found", call. = FALSE)
-  ## aRange <- .jcall(wb, "Lorg/apache/poi/hssf/usermodel/HSSFName;" ,"getNameAt", rangeIdx)
-  ## if (.jcall(aRange, "Z", "isDeleted"))
-  ##   stop(paste(range, "has no cell reference"), call. = FALSE)
-  
