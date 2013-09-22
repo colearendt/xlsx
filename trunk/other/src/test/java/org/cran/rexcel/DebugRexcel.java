31c20e98ccbb8eee1d9981e6a324d9e43b28d806
adrian.dragulescu <adrian.dragulescu@fc9b7ba1-39d8-4920-26d7-ce2cb1324a66>
2013-09-22 22:22:00 +0000

    many fixes.  Should have renamed the rexcel jar a higher version.  Did not have time.
    Still need to address other issues until the next release.

--- trunk/other/src/test/java/org/cran/rexcel/DebugRexcel.java
+++ trunk/other/src/test/java/org/cran/rexcel/DebugRexcel.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -13,23 +15,35 @@ public class DebugRexcel {
     
     public static void main(String[] args) throws InvalidFormatException, IOException {
 //        FileInputStream in = new FileInputStream(new File("C:/temp/fca3_monthly_ob_v2.xls") );
-//        Workbook wb = WorkbookFactory.create(in);
-//        Sheet sheet = wb.getSheetAt(0); 
-//
-//        RInterface R = new RInterface();
+        FileInputStream in = new FileInputStream(new File("/home/adrian/Documents/rexcel/trunk/resources/Mappe6.xlsx") );
+        Workbook wb = WorkbookFactory.create(in);
+        Sheet sheet = wb.getSheetAt(0); 
+        
+        Row row = sheet.getRow(37);
+        Cell cell = row.getCell(0);
+        
+        System.out.println( cell.getCellType() );
+        
+        System.out.println( cell.getNumericCellValue() );  // returns 8561807.0 !!  I want "8561807"
+        
+
+        RInterface R = new RInterface();
 //
 //        int maxRow = sheet.getLastRowNum();
 //        System.out.println(maxRow);
 //        
 //        //String[] res = R.readRowStrings(sheet, 0, 10, 0);
-//        String[] res = R.readColStrings(sheet, 2075, 2080, 0);
-//        //double[] res = R.readColDoubles(sheet, 0, 2006, 0);
-//        for (int i=0; i<res.length; i++){
-//          System.out.println(res[i]);
-//        }
+        String[] res = R.readColStrings(sheet, 35, 38, 0);
+        //double[] res = R.readColDoubles(sheet, 0, 2006, 0);
+        for (int i=0; i<res.length; i++){
+          System.out.println(res[i]);
+        }
 //        
-        TestRInterface ti = new TestRInterface();
+        //TestRInterface ti = new TestRInterface();
         //ti.issue13();
+    	
+    	
+    	
         
       }
     
