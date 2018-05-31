2edf26cdae804fb97f53baf7641d95e81ef7c36e
Cole Arendt <cole.arendt@outlook.com>
2018-05-31 04:49:28 +0000

    remove strange scaffolding

--- tests/testthat/test-aaa_setup.R
+++ tests/testthat/test-aaa_setup.R
@@ -1,7 +1,3 @@
 
 ## Remove tmp directory if already present
 remove_tmp()
-
-## Development version - add classPath
-ijava <- rprojroot::is_r_package$find_file('inst/java')
-rJava::.jaddClassPath(dir(ijava, full.names = TRUE))
