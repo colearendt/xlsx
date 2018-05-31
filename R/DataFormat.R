eee40653c104ef068a99cad6eebff09a007c13d7
Cole Arendt <cole.arendt@outlook.com>
2018-05-31 10:48:26 +0000

    switch dataformat order

--- R/DataFormat.R
+++ R/DataFormat.R
@@ -1,3 +1,10 @@
+######################################################################
+# Deal with Format properties for cells, needed for CellStyle
+#' @export
+#' @rdname DataFormat
+is.DataFormat <- function(df) inherits(df, "DataFormat")
+
+
 
 ######################################################################
 # Create a DataFormat
@@ -40,10 +47,3 @@ DataFormat <- function(x)
   structure(list(dataFormat=as.character(x)),
     class="DataFormat")
 }
-
-######################################################################
-# Deal with Format properties for cells, needed for CellStyle
-#' @export
-#' @rdname DataFormat
-is.DataFormat <- function(df) inherits(df, "DataFormat")
-
