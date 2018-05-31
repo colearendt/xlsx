5d35180ae2da31a644b61cfd6985926b148d57f6
Cole Arendt <cole.arendt@outlook.com>
2018-05-31 04:36:58 +0000

    fix rJava deps

--- tests/testthat/helper-format.R
+++ tests/testthat/helper-format.R
@@ -1,16 +1,16 @@
 read_data_format <- function(cell) {
   cs <- getCellStyle(cell)
   
-  .jcall(cs,'S','getDataFormatString')
+  rJava::.jcall(cs,'S','getDataFormatString')
 }
 
 read_fill_foreground <- function(cell) {
   cs <- getCellStyle(cell)
   
-  col <- .jcall(cs,'Lorg/apache/poi/xssf/usermodel/XSSFColor;','getFillForegroundXSSFColor')
+  col <- rJava::.jcall(cs,'Lorg/apache/poi/xssf/usermodel/XSSFColor;','getFillForegroundXSSFColor')
   
   if (!is.null(col)) {
-    barr <- .jcall(col,'[B','getRgb')
+    barr <- rJava::.jcall(col,'[B','getRgb')
     return(paste(as.character(barr),collapse=''))
   } else {
     return(NULL)
