6b6a04b16c4bc542db43da11633db60d8e7da515
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2018-08-27 12:25:37 -0400

    Fix bug in java versions >9
    I think Java is up to double digit versions now. For example, `R CMD javareconf` produces:
    Java version: 10.0.2
    
    It looks like line 46:
    
    ```
    if (jversion < "1.5.0")stop(...)
    ```
    
     calls a simple `<` to check versions which does not parse this correctly. For example `"10.0.2" < "1.0.6"` evaluates to `TRUE` in R. It looks like the `utils` package has a `compareVersion` function which seems to evaluate correctly e.g. `utils::compareVersion(jversion,"1.5.0")` returns 1. 
    
    It looks like this package already has dependencies on `utils` so would this be a simple fix without adding any additional dependencies?

--- R/utilities.R
+++ R/utilities.R
@@ -43,7 +43,7 @@
   
   # what's your java  version?  Need > 1.5.0.
   jversion <- .jcall('java.lang.System','S','getProperty','java.version')
-  if (jversion < "1.5.0")
+  if (utils::compareVersion(jversion,"1.5.0")<0)
     stop(paste("Your java version is ", jversion,
                  ".  Need 1.5.0 or higher.", sep=""))
   
