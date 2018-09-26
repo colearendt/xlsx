806141429e013f0445dec032cfcae627dc01d311
Cole Arendt <cole@rstudio.com>
2018-09-26 05:05:33 +0000

    add tests for saving a workbook

--- tests/testthat/test-Workbook.R
+++ tests/testthat/test-Workbook.R
@@ -4,6 +4,24 @@ test_that('fails on incorrect type', {
   expect_error(createWorkbook('mine'),'Unknown format')
 })
 
+test_that("saves a workbook", {
+  wb <- createWorkbook()
+  tf <- fs::file_temp("save", ext = ".xlsx")
+  
+  saveWorkbook(wb, tf)
+  wb_read <- loadWorkbook(tf)
+})
+
+test_that("saves a workbook with password", {
+  skip("password currently broken")
+  wb <- createWorkbook()
+  pwd <- "password"
+
+  tf <- fs::file_temp("save-pass", ext = ".xlsx")
+  saveWorkbook(wb, tf, password = pwd)
+  wb_read <- loadWorkbook(tf, password = pwd)
+})
+
 context('getSheets')
 
 test_that('returns null with no sheets', {
@@ -12,3 +30,5 @@ test_that('returns null with no sheets', {
   expect_null(suppressMessages(getSheets(wb)))
   expect_output(getSheets(wb),'no sheets')
 })
+
+
