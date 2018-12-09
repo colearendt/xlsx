74f78e6c451e72d1e52d0ec4bed0ba1d785ef358
Cole Arendt <cole@rstudio.com>
2018-12-09 19:21:56 +0000

    fix compare version

--- R/utilities.R
+++ R/utilities.R
@@ -42,8 +42,8 @@
   rJava::.jpackage(pkgname, lib.loc = libname)  # needed to load RInterface.java
   
   # what's your java  version?  Need > 1.5.0.
-  jversion <- rJava::.jcall('java.lang.System','S','getProperty','java.version')
-  if (jversion < "1.5.0")
+  jversion <- .jcall('java.lang.System','S','getProperty','java.version')
+  if (utils::compareVersion(jversion,"1.5.0") < 0)
     stop(paste("Your java version is ", jversion,
                  ".  Need 1.5.0 or higher.", sep=""))
   
