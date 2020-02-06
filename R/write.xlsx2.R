aa6d8c162dfe7e91ecfbc3d0a3124a70560b00c6
Cole Arendt <cole@rstudio.com>
2020-02-06 02:12:58 +0000

    switch write.xlsx2 to roxygen

--- R/write.xlsx2.R
+++ R/write.xlsx2.R
@@ -1,7 +1,9 @@
-# Write a data.frame to a new xlsx file. 
+# Write a data.frame to a new xlsx file.
 # with a java back-end
 #
 
+#' @export
+#' @rdname write.xlsx
 write.xlsx2 <- function(x, file, sheetName="Sheet1",
   col.names=TRUE, row.names=TRUE, append=FALSE,
   password=NULL, ...)
@@ -11,15 +13,15 @@ write.xlsx2 <- function(x, file, sheetName="Sheet1",
   } else {
     ext <- gsub(".*\\.(.*)$", "\\1", basename(file))
     wb  <- createWorkbook(type=ext)
-  }  
+  }
   sheet <- createSheet(wb, sheetName)
 
   addDataFrame(x, sheet, col.names=col.names, row.names=row.names,
     startRow=1, startColumn=1, colStyle=NULL, colnamesStyle=NULL,
     rownamesStyle=NULL)
 
-  saveWorkbook(wb, file, password=password)  
-  
+  saveWorkbook(wb, file, password=password)
+
   invisible()
 }
 
