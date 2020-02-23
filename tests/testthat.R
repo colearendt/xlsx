74967629d21c0f363cd52079dcc50cbe300ef06c
Cole Arendt <cole.arendt@outlook.com>
2020-02-23 16:27:23 -0500

    patch temp directory set default java temp directory to R temp directory fix CRAN tmp directory cleanup issue

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,13 @@
 library(testthat)
 library(xlsx)
 
+invisible(
+  rJava::.jcall(
+    "java/lang/System",
+    "Ljava/lang/String;",
+    "setProperty",
+      "java.io.tmpdir", tempdir()
+    )
+)
+
 test_check("xlsx")
