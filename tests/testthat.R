0b85ada7980cce79f4c37f2433f9be0e60a4b1de
Cole Arendt <cole.arendt@outlook.com>
2020-02-24 07:02:25 -0500

    remove setting java tempdir in test bootstrapping it should happen on library load now

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,13 +1,4 @@
 library(testthat)
 library(xlsx)
 
-invisible(
-  rJava::.jcall(
-    "java/lang/System",
-    "Ljava/lang/String;",
-    "setProperty",
-      "java.io.tmpdir", tempdir()
-    )
-)
-
 test_check("xlsx")
