da52ff485c4dff6ddf3bfcdb0202a49abd5756c3
Cole Arendt <cole.arendt@outlook.com>
2022-01-29 21:03:14 -0500

    fix whitespace

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -15,25 +15,25 @@ context('Memory Crash')
 test_that('crashes with memory volume', {
   skip('do not worry at present')
   orig <- getOption('java.parameters')
-  
-  options(java.parameters="-Xmx4096m")  
+
+  options(java.parameters="-Xmx4096m")
   require(xlsx)
-  
+
   N <- 500    # crash with N=5000, at sheet 20
-  
+
   x <- as.data.frame(matrix(1:N, nrow=N, ncol=59))
-  
+
   wb <- createWorkbook()
   for (k in 1:100) {
     cat("On sheet", k, "\n")
     sheetName <- paste("Sheet", k, sep="")
     sheet <- createSheet(wb, sheetName)
-    
+
     addDataFrame(x, sheet)
   }
-  
+
   saveWorkbook(wb, test_tmp("junk.xlsx"))
-  
+
   options(java.parameters = orig)
 })
 
